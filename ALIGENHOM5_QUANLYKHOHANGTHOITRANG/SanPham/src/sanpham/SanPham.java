dfd3848ce71be2dced61219688459421d7f54db4
Dungtt211203 <dungttph25462@fpt.edu.vn>
2023-11-22 22:50:49 +0700

    update San Pham

---
+++ ALIGENHOM5_QUANLYKHOHANGTHOITRANG/SanPham/src/sanpham/SanPham.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sanpham;
+
+/**
+ *
+ * @author Admin
+ */
+public class SanPham {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
