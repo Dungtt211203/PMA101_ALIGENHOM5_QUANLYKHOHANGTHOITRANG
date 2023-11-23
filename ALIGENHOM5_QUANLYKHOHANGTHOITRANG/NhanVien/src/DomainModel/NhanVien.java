7af6980ea11e87c22633883f094b448b29e8cab5
ThanhPH45763 <thanhnvph45763@fpt.edu.vn>
2023-11-23 21:51:14 +0700

    NhanVien
    PH45763- Thanh

---
+++ ALIGENHOM5_QUANLYKHOHANGTHOITRANG/NhanVien/src/DomainModel/NhanVien.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DomainModel;
+
+/**
+ *
+ * @author NamNguyenTien
+ */
+public class NhanVien {
+    private String id;
+    private String ma;
+    private String ten, tenDem, ho;
+    private String ngaySinh;
+    private int gioiTinh;
+    private String sdt;
+    private String diaChi;
+    private int trangThai;
+
+    public NhanVien(String id, String ma, String ten, String tenDem, String ho, String ngaySinh, int gioiTinh, String sdt, String diaChi, int trangThai) {
+        this.id = id;
+        this.ma = ma;
+        this.ten = ten;
+        this.tenDem = tenDem;
+        this.ho = ho;
+        this.ngaySinh = ngaySinh;
+        this.gioiTinh = gioiTinh;
+        this.sdt = sdt;
+        this.diaChi = diaChi;
+        this.trangThai = trangThai;
+    }
+
+    public NhanVien() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMa() {
+        return ma;
+    }
+
+    public void setMa(String ma) {
+        this.ma = ma;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getTenDem() {
+        return tenDem;
+    }
+
+    public void setTenDem(String tenDem) {
+        this.tenDem = tenDem;
+    }
+
+    public String getHo() {
+        return ho;
+    }
+
+    public void setHo(String ho) {
+        this.ho = ho;
+    }
+
+    public String getNgaySinh() {
+        return ngaySinh;
+    }
+
+    public void setNgaySinh(String ngaySinh) {
+        this.ngaySinh = ngaySinh;
+    }
+
+    public int getGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(int gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public int getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(int trangThai) {
+        this.trangThai = trangThai;
+    }
+
+    
+    
+
+    @Override
+    public String toString() {
+        return "NhanVien{" + "id=" + id + ", ma=" + ma + ", ten=" + ten + ", tenDem=" + tenDem + ", ho=" + ho + ", ngaySinh=" + ngaySinh + ", gioiTinh=" + gioiTinh + ", sdt=" + sdt + ", diaChi=" + diaChi + ", trangThai=" + trangThai + '}';
+    }
+
+    
+    
+    
+}
