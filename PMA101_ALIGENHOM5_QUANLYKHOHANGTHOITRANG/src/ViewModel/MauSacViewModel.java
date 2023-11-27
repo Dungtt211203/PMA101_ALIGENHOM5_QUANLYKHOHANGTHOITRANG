3fb36d6ded0e5d301301cf3bb2aea998d5561bc7
Dungtt211203 <dungttph25462@fpt.edu.vn>
2023-11-27 22:38:07 +0700

    update mausac_ph25462

---
+++ PMA101_ALIGENHOM5_QUANLYKHOHANGTHOITRANG/src/ViewModel/MauSacViewModel.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ViewModel;
+
+/**
+ *
+ * @author Admin
+ */
+public class MauSacViewModel {
+    private String id;
+    private String maMS;
+    private String tenMS;
+
+    public MauSacViewModel() {
+    }
+
+    public MauSacViewModel(String id, String ma, String ten) {
+        this.id = id;
+        this.maMS = ma;
+        this.tenMS = ten;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMa() {
+        return maMS;
+    }
+
+    public void setMa(String ma) {
+        this.maMS = ma;
+    }
+
+    public String getTen() {
+        return tenMS;
+    }
+
+    public void setTen(String ten) {
+        this.tenMS = ten;
+    }
+
+    @Override
+    public String toString() {
+        return tenMS;
+    }
+}
